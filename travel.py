d29a57bb8968edf20cb07a38774f615613299f9a
bfafata <70115786+bfafata@users.noreply.github.com>
2021-08-28 14:09:26 -0400

    dsa

--- travel.py
+++ travel.py
@@ -1,8 +1,6 @@
 import requests
 import json
 def time(org,des):
-    if des==0:
-        return 0
     data={"origin":org,"destination":des,"mode":"walking","key":"AIzaSyDTgcZY0M3Ma7O49qFmhq4d-4mGpv3WBU4"}
     testresponse=requests.get("https://maps.googleapis.com/maps/api/directions/json",params=data)
     testjson=json.loads(testresponse.text)
